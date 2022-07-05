45cd49a59e753523928e27847468ebffff10f214
Sahai Jordi Alan A <73820349+SJAlanA@users.noreply.github.com>
2022-07-05 21:59:08 +0530

    Add files via upload

---
+++ PasswordGenerator.py
@@ -0,0 +1,11 @@
+import string
+import random
+a2b = string.ascii_lowercase
+A2B = string.ascii_uppercase
+nums = string.digits
+syms = string.punctuation
+length = int(input("Password Length : "))
+characters = list(a2b) + list(A2B) + list(nums) + list(syms)
+random.shuffle(characters)
+print("Password : ")
+print("".join(random.sample(characters, length)))
